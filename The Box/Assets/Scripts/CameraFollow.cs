0966852758490b7d1fd8973a21492a8ad41ca24e
Wesly Barayuga <86134875+wes-brook@users.noreply.github.com>
2022-08-27 23:38:15 -0700

    Fixed Lighting
    -Added generated lightmap
    
    -Working on adding smooth camera following to player

---
+++ The Box/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿/* using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+
+    void LateUpdate() {
+        transform.position.z += target.position.z;
+    }
+}
+ */
